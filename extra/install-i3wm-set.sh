17f05699c3d358e7c1a2ca7e75b9d1b3b0da2a62
henryust12 <henryust12@gmail.com>
2022-10-22 08:01:52 +0900

    added i3wm set installer and nerd font complete installer

---
+++ extra/install-i3wm-set.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+paru -S i3-wm
+paru -S dmenu
+paru -S polybar
+paru -S i3status-rust
+# i3status-rust dependencies
+paru -S ttf-font-awesome kdeconnect powerline-fonts iw alsa-utils
+paru -S rofi
---
+++ extra/install-nerd-fonts-complete.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+paru -S nerd-fonts-complete
